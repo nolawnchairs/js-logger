e68495e7e128a6cbdd660d955f1d4253f7652bfa
mwieczorek <nolawnchairs@gmail.com>
2020-02-01 14:53:08 +0700

    compiled

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 const logger = require(".")
 const log = logger("[test-app]", 'debug', { printLevel: 'initial' })
+const log2 = logger('[test-app-2', 'debug', { printLevel: 'full' })
 
 log.debug("This is a debugger message")
 log.info("This is an info message")
 log.warn("This is a warning message")
-log.error("This is an error message")
+log2.error("This is an error message")
